8e6a17380f95c04adbbbe4c619b7b8765162b707
idawud <ismaildawud96@gmail.com>
2019-12-03 14:53:41 +0000

    completed main-page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomersComponent } from './components/customers/customers.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { MainComponent } from './components/main/main.component';
+import { ReportingComponent } from './components/reporting/reporting.component';
+import { NewprojectComponent } from './components/newproject/newproject.component';
+import { NewcustomerComponent } from './components/newcustomer/newcustomer.component';
 
 
 const routes: Routes = [
-  { path : '', component : CustomersComponent },
+  { path : '', component : MainComponent },
   { path : 'customers', component : CustomersComponent },
   { path : 'projects', component : ProjectsComponent },
-  { path : 'newcustomer', component : CustomersComponent },
-  { path : 'newproject', component : CustomersComponent },
-  { path : 'reporting', component : CustomersComponent }
+  { path : 'newcustomer', component : NewcustomerComponent },
+  { path : 'newproject', component : NewprojectComponent },
+  { path : 'reporting', component : ReportingComponent }
 ];
 
 @NgModule({
