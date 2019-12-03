3df2e7efb435a6cf25d279d06fd405e9b1d8c343
idawud <ismaildawud96@gmail.com>
2019-12-03 17:41:06 +0000

    data injection

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  projects = [
+    {id : 1, name : 'TCMS2.0', description: 'Turntabl Management System 2.0'},
+    {id : 2, name : 'Trober', description: 'Uber service for trotros in Ghana'},
+    {id : 3, name : 'LAB Management System', description: 'Laboratory Manangement System for Madina Clinic'},
+    {id : 4, name : 'Free Ride', description: 'App to free ride from friends and stranger on the same commute route'}
+  ];
   constructor() { }
 
   ngOnInit() {
