68bb68e0d50406fb66ff40715a15cd3234d64707
idawud <ismaildawud96@gmail.com>
2019-12-03 14:37:46 +0000

    added all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomersComponent } from './components/customers/customers.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', component : CustomersComponent },
+  { path : 'customers', component : CustomersComponent },
+  { path : 'projects', component : ProjectsComponent },
+  { path : 'newcustomer', component : CustomersComponent },
+  { path : 'newproject', component : CustomersComponent },
+  { path : 'reporting', component : CustomersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { NewprojectComponent } from './components/newproject/newproject.component';
+import { NewcustomerComponent } from './components/newcustomer/newcustomer.component';
+import { ReportingComponent } from './components/reporting/reporting.component';
+import { MainComponent } from './components/main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    ProjectsComponent,
+    NewprojectComponent,
+    NewcustomerComponent,
+    ReportingComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
