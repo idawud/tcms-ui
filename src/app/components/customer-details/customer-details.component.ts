5d848e756f1e338e3a178fdb7fc3da3479b99b6c
idawud <ismaildawud96@gmail.com>
2019-12-04 08:41:00 +0000

    added customerDetails page

--- src/app/components/customer-details/customer-details.component.ts
+++ src/app/components/customer-details/customer-details.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-details.component.css']
 })
 export class CustomerDetailsComponent implements OnInit {
+  customer = { id : 1, name: 'Dawud Ismail', address: 'North Legon Libya', telephoneNumber: '057652365456', email: 'Email@me.dot' };
 
   constructor() { }
 
